26b04630037b4a7f078542f9d992b719740e5a08
Hakan GÜZEL <hakan-guzel@outlook.com>
2020-06-14 12:53:21 +0300

    Initial commit

---
+++ config/db.config.js
@@ -0,0 +1,15 @@
+'use strict';
+const mysql = require('mysql');
+
+
+const dbConn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'ExpressJsRestFullApi'
+});
+dbConn.connect(function(err) {
+  if (err) throw err;
+  console.log("Database Connected!");
+});
+module.exports = dbConn;
